819e0d688f3b791f8b9d8e5be267f8c8f1dc12d5
anno <zhenglfsir@gmail.com>
2016-09-19 21:43:35 +0800

    2

--- app/schemas/comment.js
+++ app/schemas/comment.js
@@ -6,12 +6,29 @@ var CommentSchema = new moogoose.Schema({
 	movie: {type: ObjectId, ref: 'Movie'},
 	from: {type: ObjectId, ref: 'User'},
 	to: {type: ObjectId, ref: 'User'},
-	Content: String,
+	content: String,
 	meta: {
 		createAt: {
+			type: Date,
+			default: Date.now()
+		},
+		updateAt: {
 			type: Date,
 			default: Date.now()
 		}
 	}
 
 }); 
+
+CommentSchema.pre('save', function(next) {
+	if(this.isNew) {
+
+	}
+});
+
+
+
+
+
+
+
